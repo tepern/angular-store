67ad96337b75e864a6d0564554dec75a0f320c1b
Natalia <teper.n@gmail.com>
2022-02-17 16:02:04 +0400

    Main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './home/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
