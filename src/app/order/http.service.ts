c64add6ca1b441a2897fb6818ff951086de11a27
Natalia <teper.n@gmail.com>
2022-03-12 02:41:49 +0400

    V-03 HttpClient implementation instead fetch

--- src/app/order/http.service.ts
+++ src/app/order/http.service.ts
@@ -11,41 +11,24 @@ import { environment } from 'src/environments/environment';
 })
 
 export class HttpService {
-  public error$: Subject<any> = new Subject<any>();
+  public error$: Subject<HttpErrorResponse> = new Subject<HttpErrorResponse>();
 
   constructor(private http: HttpClient) {
   }
 
   getData(page: number, limit: number):Observable<Model[]> {
-        
-    const $data = fetch('http://localhost:4200/api/db/car?page=' + (page-1) + '&limit=' + limit,
-      {
-        headers: {
-          'X-Api-Factory-Application-Id': '5e25c641099b810b946c5d5b'
-        },
-        method: 'GET', // GET, POST, PUT, DELETE
-      }
 
-    ).then(response => {
-        if (response.status !=200) {
-          return null;
-        } else {
-          return response.json();
-        }
-      },
-      failResponse => {
-        catchError(this.handleError.bind(this));
-        return null;
-    });
-    
-    return from($data).pipe(map((data:any) => {
+    const apiHeaders = new HttpHeaders().set('X-Api-Factory-Application-Id', '5e25c641099b810b946c5d5b');
+
+    return this.http.get<Model[]>('http://localhost:4200/api/db/car?page=' + (page-1) + '&limit=' + limit, {headers: apiHeaders})
+    .pipe(map((data:any) => {
       return data["data"];
-    }));
+    }))
+    .pipe(catchError(this.handleError.bind(this)));      
   }
 
   private handleError(errors: HttpErrorResponse) {
     const {message, error} = errors;
-    console.log(throwError(errors));
     return throwError(errors);
   }
 }
